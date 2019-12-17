9180e7d82b2cea23d567035120c7ed36d86c119b
Yann Legendre <yann.legendre@essec.edu>
2019-12-17 10:01:59 +0100

    refactor : separate user logic from application controller logic

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,8 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: current_email)
+    @todos = current_user.todos
+    # @todos = Todo.where(email: current_email)
   end
 
   def new
@@ -10,7 +11,8 @@ class TodosController < ApplicationController
   end
 
   def create
-    Todo.create(todo_params.merge(email: current_email))
+    current_user.todos.create(todo_params)
+    # Todo.create(todo_params.merge(email: current_email))
     redirect_to todos_path
   end
 
