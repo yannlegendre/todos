8867d7b4b513b4d5453e7135846b978aa568514a
Yann Legendre <yann.legendre@essec.edu>
2019-12-17 09:51:51 +0100

    refactor sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
   def signed_in
-    session[:current_email].present?
+    current_email.present?
   end
+
+  def sign_in_as(email)
+    session[:current_email] = email
+  end
+
+
+  def current_email
+    session[:current_email]
+  end
+
 end
